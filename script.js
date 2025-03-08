2bfb9aa44a4386992689574868cd2cbfb5ff17c8
alexl53 <alexander88lee@gmail.com>
2025-03-08 20:59:01 +0000

    done

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function fix() {
     //Write your code here!
+    let coconut = document.getElementById("coconut");
+    coconut.remove();
+    let coast = document.getElementById("coast");
+    coast.remove();
+    let watermelon = document.getElementById("watermelon");
+    watermelon.remove();
+    let sunflowers = document.getElementById("sunflowers");
+    sunflowers.remove();
+    let tree = document.createElement("img");
+    tree.id = "tree";
+    tree.src = "to-add/christmasTree.jpg";
+    photos = document.getElementById("photos");
+    photos.appendChild(tree);
+    let snowman = document.createElement("img");
+    snowman.id = "snowman";
+    snowman.src = "to-add/snowmen.jpg";
+    photos.appendChild(snowman);
+    let stockings = document.createElement("img");
+    stockings.id = "stockings";
+    stockings.src = "to-add/stockings.jpg";
+    photos.appendChild(stockings);
 }
